c235a5cda8e3fe39cf3c1031e6e26823a187ca2e
mechztcc <dojathan@gmail.com>
2022-04-20 11:31:43 -0300

    feat: create user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,10 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
 
 export default class UsersController {
   async store({ response, request }: HttpContextContract) {
+    const payload = request.only(['email', 'username', 'password', 'avatar']);
 
-		const body = request.body()
+    const user = await User.create(payload);
 
-    return response.status(201).send({ email: body.email })
+    return response.status(201).send(user);
   }
 }
--- tests/functional/users/create.spec.ts
+++ tests/functional/users/create.spec.ts
@@ -2,20 +2,18 @@ import { test } from '@japa/runner'
 
 test.group('Users create', () => {
   test('It should be create a user', async ({ client, assert }) => {
-    
     const payload = {
-      username: 'Alberto',
-      email: 'email@email.com',
+      username: 'Alberto2',
+      email: 'email2@email.com',
       password: '123456',
-      avatar: 'http://images.com/images/1'
+      avatar: 'http://images.com/images/1',
     }
 
-    const response = await client.post('/users').json(payload);
+    const response = await client.post('/users').json(payload)
 
-    const body = response.body();
+    const body = response.body()
 
-    response.assertStatus(201);
-    response.assertBody({ email: payload.email });
-    // assert.exists(body.email)
-  })  
+    response.assertStatus(201)
+    assert.exists(body.id);
+  })
 })
