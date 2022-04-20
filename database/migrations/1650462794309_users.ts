f0f008397d8553c787067ec4fb92c73c10454536
mechztcc <dojathan@gmail.com>
2022-04-20 11:00:52 -0300

    feat: migrating user table

--- database/migrations/1650462794309_users.ts
+++ database/migrations/1650462794309_users.ts
@@ -6,10 +6,10 @@ export default class Users extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.string('email').notNullable().unique()
+      table.string('password').notNullable()
+      table.string('username').notNullable().unique()
+      table.string('avatar').defaultTo('')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
